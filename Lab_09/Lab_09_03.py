ce87740eafdfeb92c1230c35885b5be35c2624e2
Jirayu <Jirayuphumsiri@gmail.com>
2025-09-23 22:31:23 +0700

    change lab_09_03 to pure recursive

--- Lab_09/Lab_09_03.py
+++ Lab_09/Lab_09_03.py
@@ -1,21 +1,25 @@
-def insertion_sort(data, answer=None):
+def insertion_sort(data, number, index, answer=None):
     if not data:
+        if index < len(answer) and answer[index] < number:
+            return insertion_sort(data, number, index+1, answer)
+        answer.insert(index, number)
+        print(f"insert {number} at index {index} : {answer}")
         return answer
-    number = data.pop(0)
-    i = 0
-    while i < len(answer) and answer[i] < number:
-        i += 1
-    answer.insert(i, number)
-    if not data:
-        print(f"insert {number} at index {i} : {answer} ")
-    else:
-        print(f"insert {number} at index {i} : {answer} {data}")
-    return insertion_sort(data, answer)
-    
+
+    if index < len(answer) and answer[index] < number:
+        return insertion_sort(data, number, index+1, answer)
+
+    answer.insert(index, number)
+    print(f"insert {number} at index {index} : {answer} {data}")
+
+    next_num = data[0]
+    return insertion_sort(data[1:], next_num, 0, answer)
+
+
 inp = list(map(int,input("Enter Input : ").split(" ")))
 first_list = []
 first_list.append(inp.pop(0))
-result = insertion_sort(inp, first_list)
+result = insertion_sort(inp, inp.pop(0),1,first_list)
 print("sorted")
 print(result)
 
